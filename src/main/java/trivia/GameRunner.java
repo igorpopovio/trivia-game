9e7ab1ad9c9b5106d44d870f097ac74b2c55ab37
Igor Popov <igor@igorpopov.io>
2015-02-07 16:30:07 +0200

    Prepare the code for golden master tests
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- src/main/java/trivia/GameRunner.java
+++ src/main/java/trivia/GameRunner.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 public class GameRunner {
     private static boolean notAWinner;
+    public static Random rand = new Random();
 
     public static void main(String[] args) {
         Game aGame = new Game();
@@ -12,7 +13,6 @@ public class GameRunner {
         aGame.add("Pat");
         aGame.add("Sue");
 
-        Random rand = new Random();
         do {
             aGame.roll(rand.nextInt(5) + 1);
             if (rand.nextInt(9) == 7) {
