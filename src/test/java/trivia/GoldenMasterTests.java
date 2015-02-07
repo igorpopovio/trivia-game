fa8dffc79e71d904cb348f62a10eb05291297567
Igor Popov <igor@igorpopov.io>
2015-02-07 17:30:11 +0200

    Add test to compare current run with the golden master
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- src/test/java/trivia/GoldenMasterTests.java
+++ src/test/java/trivia/GoldenMasterTests.java
@@ -1,18 +1,22 @@
 package trivia;
 
+import junitx.framework.FileAssert;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.PrintStream;
 import java.util.Random;
+
+import static java.lang.String.format;
 
 public class GoldenMasterTests {
     @Test
     @Ignore("Should be run only when generating the golden master")
     public void generateGoldenMaster() throws Exception {
         for (int seed = 0; seed < 100; seed++) {
-            String filename = String.format("golden-master/seed-%d.txt", seed);
-            System.setOut(new PrintStream(filename));
+            File goldenMasterOutput = new File(format("golden-master/seed-%d.txt", seed));
+            System.setOut(new PrintStream(goldenMasterOutput));
             runGameForSeed(seed);
         }
     }
@@ -20,5 +24,17 @@ public class GoldenMasterTests {
     public void runGameForSeed(int seed) {
         GameRunner.rand = new Random(seed);
         GameRunner.main(null);
+    }
+
+    @Test
+    public void isCurrentOutputSameAsGoldenMaster() throws Exception {
+        for (int seed = 0; seed < 100; seed++) {
+            File currentOutput = new File(format("current-output/seed-%d.txt", seed));
+            File goldenMasterOutput = new File(format("golden-master/seed-%d.txt", seed));
+            System.setOut(new PrintStream(currentOutput));
+            runGameForSeed(seed);
+
+            FileAssert.assertEquals(goldenMasterOutput, currentOutput);
+        }
     }
 }
