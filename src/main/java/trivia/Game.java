2eddf4b47232028f183b2b6ec4903ce877cd38ad
Igor Popov <igor@igorpopov.io>
2015-02-08 19:57:10 +0200

    Remove unused return value
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- src/main/java/trivia/Game.java
+++ src/main/java/trivia/Game.java
@@ -26,12 +26,11 @@ public class Game {
         }
     }
 
-    public boolean add(String playerName) {
+    public void add(String playerName) {
         players.add(playerName);
 
         System.out.println(playerName + " was added");
         System.out.println("They are player number " + players.size());
-        return true;
     }
 
     public void roll(int roll) {
